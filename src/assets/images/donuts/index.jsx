ed1d66e68109afea582c9f4283d3718eed51ec70
Kim Lu Marcelo <kimpogi2015@gmail.com>
2023-12-05 23:52:53 +0800

    add donut images

---
+++ src/assets/images/donuts/index.jsx
@@ -0,0 +1,6 @@
+import biscuits from "./biscuits.png";
+import strawberry from "./strawberry.png";
+import pink from "./pink.png";
+import chocolate from "./chocolate.png";
+
+export { biscuits, chocolate, pink, strawberry };
